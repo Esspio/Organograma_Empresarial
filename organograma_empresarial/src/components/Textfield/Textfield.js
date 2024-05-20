ba13d93bc002767eb9bbaf8d6c553bdba31bc713
Esspio <felipe.oliveira200085@gmail.com>
2024-05-20 16:23:04 -0300

    Manipulando o input do usuario

--- organograma_empresarial/src/components/Textfield/Textfield.js
+++ organograma_empresarial/src/components/Textfield/Textfield.js
@@ -1,10 +1,18 @@
 import './Textfield.css';
 
 export const Textfield = (props) => {
+
+    let valor = '';
+    const onDigitado = (event) => {
+        valor = event.target.value;
+        console.log(valor);
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder} required={props.required}></input>
+            <input placeholder={props.placeholder} required={props.required} onChange={onDigitado}
+                value={valor}></input>
         </div>
     )
 }
